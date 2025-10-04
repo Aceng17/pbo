5f87d56164f06b4e34268f118da1fea76174ef7f
Marcellino Lamusu <acenglamusu@gmail.com>
2025-10-04 11:20:50 +0700

    Create 03.js

---
+++ 03.js
@@ -0,0 +1,15 @@
+class Mahasiswa {
+  constructor(nama, nim, jurusan) {
+    this.nama = nama;
+    this.nim = nim;
+    this.jurusan = jurusan;
+  }
+
+  belajar() {
+    console.log(${this.nama} sedang belajar.);
+  }
+}
+
+// Membuat objek dari class Mahasiswa
+const mhs1 = new Mahasiswa("Marcellino Lamusu", "20240039", "Teknik Informatika");
+mhs1.belajar();
